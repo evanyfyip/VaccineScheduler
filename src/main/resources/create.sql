f57ab40bc8fc0f5b574ed4fbdd53ff7b29366ffe
Evan Yip <evan@yipsite.net>
2021-11-25 11:54:45 -0800

    Changes to create tables file

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,18 +1,43 @@
-CREATE TABLE Caregivers (
+-- Creating User table
+CREATE TABLE Users (
     Username varchar(255),
+    Name varchar(50),
     Salt BINARY(16),
     Hash BINARY(16),
     PRIMARY KEY (Username)
 );
 
+-- Creating Caregivers table
+CREATE TABLE Caregivers (
+    Username varchar(255) REFERENCES Users,
+    PRIMARY KEY (Username)
+);
+
+-- Creating Patients table
+CREATE TABLE Patients (
+    Username varchar(255) REFERENCES Users,
+    PRIMARY KEY (Username)
+);
+
+-- Creating Current availabilities table
 CREATE TABLE Availabilities (
     Time date,
     Username varchar(255) REFERENCES Caregivers,
     PRIMARY KEY (Time, Username)
 );
 
+-- Creating Vaccines table (what is in stock)
 CREATE TABLE Vaccines (
     Name varchar(255),
     Doses int,
     PRIMARY KEY (Name)
+);
+
+-- Creating Appointments table
+CREATE TABLE Appointments (
+    appointment_id INT,
+    p_username varchar(255) REFERENCES Patients(Username),
+    c_username varchar(255) REFERENCES Caregivers(Username),
+    vac_name varchar(255) REFERENCES Vaccines(Name),
+    Time date
 );
